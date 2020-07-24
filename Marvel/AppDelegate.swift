415be9b3409c7382f0fab93e0f38f30b4bfad5c5
Carlos <carlos.ruiz.cues@gmail.com>
2020-07-24 15:40:26 +0200

    Añadido MainViewController

--- Marvel/AppDelegate.swift
+++ Marvel/AppDelegate.swift
@@ -10,11 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            let mainVC = MainViewController()
+            window.rootViewController = mainVC
+            window.makeKeyAndVisible()
+        }
         return true
     }
 
---
+++ Marvel/ViewController/MainViewController/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Marvel
+//
+//  Created by MIGUEL PEREZ SUBIAS on 24/07/2020.
+//  Copyright © 2020 Carlos Ruiz Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
