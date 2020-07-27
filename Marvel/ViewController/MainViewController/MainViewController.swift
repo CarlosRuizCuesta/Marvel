71cebe1632adad910d8b2371fb43c458397fabf7
Carlos <carlos.ruiz.cues@gmail.com>
2020-07-27 19:54:42 +0200

    Preparacion del buscador de la pantalla principal

--- Marvel/ViewController/MainViewController/MainViewController.swift
+++ Marvel/ViewController/MainViewController/MainViewController.swift
@@ -9,27 +9,68 @@
 import UIKit
 
 class MainViewController: UIViewController {
+
+    @IBOutlet weak var searchBar : UISearchBar!
+    @IBOutlet weak var lblName : UILabel!
+    @IBOutlet weak var lblDesc : UILabel!
+    @IBOutlet weak var btnMoreInfo : UIButton!
+    @IBOutlet weak var imageView : UIImageView!
     
-    @IBOutlet weak var textField : UITextField!
-    @IBOutlet weak var button : UIButton!
+    
+    var hero : Hero!
+    let leftBarButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: nil)
+        barButtonItem.tintColor = UIColor.red
+        return barButtonItem
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        button.addTarget(self, action: #selector(click_button), for: .touchUpInside)
-        
-        MarvelApiHeroes(delegate: self, name: "iron man").start()
-        // Do any additional setup after loading the view.
+        btnMoreInfo.addTarget(self, action: #selector(click_button), for: .touchUpInside)
+        searchBar.delegate = self
+        setupData()
+    }
+    
+    func setupData() {
+        if let hero = hero {
+            if !hero.name.isEmpty {
+                searchBar.text = hero.name
+            }
+        }
+    }
+    
+    func completeData() {
+        showItems()
+        lblName.text = hero.name
+        lblDesc.text = hero.description
+    }
+    
+    func showItems() {
+        lblName.isHidden = false
+        lblDesc.isHidden = false
+        imageView.isHidden = false
+        btnMoreInfo.isHidden = false
     }
     
     @objc func click_button(sender : UIButton) {
+        let moreInfoControllerVC = MoreInfoController(nibName: "MoreInfoController", bundle: nil)
+        let navigation  = UINavigationController(rootViewController:moreInfoControllerVC)
+        moreInfoControllerVC.hero = hero
+        navigation.modalPresentationStyle = .fullScreen
+        navigation.modalTransitionStyle = .crossDissolve
+        self.present(navigation, animated: true, completion: nil)
+    }
+}
+
+extension MainViewController : UISearchBarDelegate {
+    // When button "Search" pressed
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
         
-        if let nombre = textField.text {
-            if !nombre.isEmpty {
-                MarvelApiHeroes(delegate: self, name: nombre).start()
+        if let text = searchBar.searchTextField.text {
+            if !text.isEmpty {
+                MarvelApiHeroes(delegate: self, name: text).start()
             }
         }
-        
     }
 }
 
@@ -38,12 +79,8 @@ extension MainViewController : MarvelApiResponse {
     
     func response(hero: Hero) {
         DispatchQueue.main.async {
-            let moreInfoControllerVC = MoreInfoController(nibName: "MoreInfoController", bundle: nil)
-            let navigation  = UINavigationController(rootViewController:moreInfoControllerVC)
-            moreInfoControllerVC.hero = hero
-            navigation.modalPresentationStyle = .fullScreen
-            navigation.modalTransitionStyle = .crossDissolve
-            self.present(navigation, animated: true, completion: nil)
+            self.hero = hero
+            self.completeData()
         }
     }
     
