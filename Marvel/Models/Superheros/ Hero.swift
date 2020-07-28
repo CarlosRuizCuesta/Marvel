3051685b4bd5bb6beae86f35de122b204d954f42
Carlos <carlos.ruiz.cues@gmail.com>
2020-07-28 12:55:41 +0200

    BUGFIX: Comprueba si existe available antes de usarlo

--- Marvel/Models/Superheros/ Hero.swift
+++ Marvel/Models/Superheros/ Hero.swift
@@ -30,38 +30,52 @@ class Hero {
         var sections : [Appearance] = []
         
         if let comics = comics {
-            if comics.available > 0 {
-                sections.append(comics)
+            
+            if comics.available != nil {
+                if comics.available > 0 {
+                    sections.append(comics)
+                }
             }
         }
         
         if let series = series {
-            if series.available > 0 {
+            if series.available != nil {
+                if series.available > 0 {
                 sections.append(series)
+                }
+                
             }
         }
         
         if let stories = stories {
-            if stories.available > 0 {
-                sections.append(stories)
+            if stories.available != nil {
+                if stories.available > 0 {
+                    sections.append(stories)
+                }
             }
         }
         
         if let events = events {
-            if events.available > 0 {
-                sections.append(events)
+            if events.available != nil {
+                if events.available > 0 {
+                    sections.append(events)
+                }
             }
         }
         
         if let creators = creators {
-            if creators.available > 0 {
-                sections.append(creators)
+            if creators.available != nil {
+                if creators.available > 0 {
+                    sections.append(creators)
+                }
             }
         }
         
         if let characters = characters {
-            if characters.available > 0 {
-                sections.append(characters)
+            if characters.available != nil {
+                if characters.available > 0 {
+                    sections.append(characters)
+                }
             }
         }
         
